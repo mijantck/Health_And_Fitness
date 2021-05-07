7dad6603afedab7393180248f5779487cebe80f0
mijantck <mijantck3.ofic@gmail.com>
2021-05-07 11:44:08 +0600

    Final

---
+++ app/src/main/java/com/mrsoftit/homefit/WalkandStep/receivers/HardwareStepCountReceiver.java
@@ -0,0 +1,24 @@
+
+package com.mrsoftit.homefit.WalkandStep.receivers;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.legacy.content.WakefulBroadcastReceiver;
+
+import android.util.Log;
+
+import com.mrsoftit.homefit.WalkandStep.services.HardwareStepCounterService;
+
+
+
+public class HardwareStepCountReceiver extends WakefulBroadcastReceiver {
+    private static final String LOG_CLASS = HardwareStepCountReceiver.class.getName();
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.i(LOG_CLASS, "Received hardware step count alarm");
+        Intent serviceIntent = new Intent(context, HardwareStepCounterService.class);
+        context.startService(serviceIntent);
+    }
+}
